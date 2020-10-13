a232d3d3cb6fa25cff245157002bd134daf1b892
99Gran <nini.g94@gmail.com>
2020-10-13 10:13:10 +0200

    js commit

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 window.addEventListener('wheel', handleScroll);
 
+let frame = 0
+
 function handleScroll(event) {
     //event.deltaY;
     let productElm = document.querySelector('.koppdemo');
-    productElm.style.cssText = "border: 5px solid black;";
+    productElm.style.cssText = "background-position-x: calc("+ Math.floor(frame) +" * -300px);";
+    frame +=0.1;
+    if(frame > 5) frame = 0;
 }
